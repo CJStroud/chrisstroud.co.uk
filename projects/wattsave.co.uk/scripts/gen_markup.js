ce0c18a1cc1b750f2ca63ab1aa0a91fadeb95c87
Chris Stroud <chris.stroud00@gmail.com>
2018-07-07 22:44:14 +0100

    Generate product html files

--- projects/wattsave.co.uk/scripts/gen_markup.js
+++ projects/wattsave.co.uk/scripts/gen_markup.js
@@ -1,8 +1,10 @@
 var handlebars = require('handlebars')
 var layouts = require('handlebars-layouts')
 var fs = require('fs')
+const chalk = require('chalk')
+const figures = require('figures')
 
-var data = require('../data/products.json')
+var products = require('../data/products.json')
 
 // Register helpers
 handlebars.registerHelper(layouts(handlebars))
@@ -13,7 +15,7 @@ handlebars.registerPartial('layout', fs.readFileSync('src/layout.hbs', 'utf8'))
 var indexTemplate = handlebars.compile(fs.readFileSync('src/pages/index.html', 'utf8'))
 const index = indexTemplate({
   title: 'Home',
-  items: data
+  products: products
 })
 
 var aboutTemplate = handlebars.compile(fs.readFileSync('src/pages/about.html', 'utf8'))
@@ -28,11 +30,30 @@ const pages = {
   'contact.html': contact
 }
 
+var productTemplate = handlebars.compile(fs.readFileSync('src/pages/product.html', 'utf8'))
+
+// Add a page for each product in products.json
+products.forEach(product => {
+  const templateData = product
+  templateData.title = product.name
+  pages[product.href] = productTemplate(templateData)
+})
+
 Object.entries(pages).map(([path, template]) => {
+  const pathParts = path.split('/')
+  const dir = pathParts.length > 1 ? pathParts.slice(0, pathParts.length - 1).join('/') : null
+  if (dir && !fs.existsSync(dir)) {
+    fs.mkdirSync(dir)
+  }
+
   fs.writeFile(path, template, function (err) {
     if (err) {
+      const icon = chalk.red(figures.cross)
+      console.log(`${icon} Error generating ${path}`)
       console.log(err)
+    } else {
+      const icon = chalk.green(figures.tick)
+      console.log(`${icon} Generated ${path}`)
     }
-    console.log(`${path} created`)
   })
 })
